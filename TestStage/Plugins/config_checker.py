c29757104ddf04ae67b3129553841e508c45bddb
mathiasr <mathias@sangu.de>
2016-07-22 08:44:51 +0200

    - Teststage - strting behaviour with drag and drop

--- TestStage/Plugins/config_checker.py
+++ TestStage/Plugins/config_checker.py
@@ -1,5 +1,27 @@
 """This module checks on consistency of config file"""
+import hashlib
 
-class checker():
-    def __init__(self):
-        pass
+
+# ----------------------------------------------------------------------------------------------------------------------
+class config_checker():
+    def __init__(self, config_test, config_orig):
+        self.config_test = config_test
+        self.config_orig = config_orig
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def load_hash(self, file):
+
+        hash_number = hashlib.sha256()
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def checksum_md5(self, filename):
+        md5 = hashlib.md5()
+        with open(filename, 'rb') as f:
+            for chunk in iter(lambda: f.read(8192), b''):
+                md5.update(chunk)
+
+        return md5.digest()
+
+
+CC = config_checker('config_checker.py', 'config_checker.py')
+print CC.checksum_md5('config_checker.py')
