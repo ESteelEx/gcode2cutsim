0f694861c982199852fd328d722b066144f4d43e
mathiasr <mathias@sangu.de>
2016-09-22 23:49:23 +0200

    - param guard

--- RhinoInterface/ParameterGuard_UI/guard.py
+++ RhinoInterface/ParameterGuard_UI/guard.py
@@ -5,33 +5,40 @@ from watchdog.events import FileSystemEventHandler
 
 # ----------------------------------------------------------------------------------------------------------------------
 class ParamEventHandler(FileSystemEventHandler):
+    def __init__(self, pluginPath, corePath, PG_UI):
+        self.PG_UI = PG_UI
+
     def on_modified(self, event):
-        #keys = event.key
-        # isdir = event.is_directory
         src_path = event.src_path
         etype = event.event_type
 
         if etype == 'modified' and src_path.split('\\')[-1] == 'Mesh.ini':
             print 'Config file was modified. Updating UI.'
+            self.PG_UI.editbox[0].SetValue('Hey')
 
 
 # ----------------------------------------------------------------------------------------------------------------------
 class guard_of_changes(threading.Thread):
-    def __init__(self, pluginPath, corePath):
+    def __init__(self, pluginPath, corePath, PG_UI):
         self.pluginPath = pluginPath
         self.corePath = corePath
         self.runstat = True
+        self.PG_UI = PG_UI
         threading.Thread.__init__(self)
 
     # ------------------------------------------------------------------------------------------------------------------
     def run(self):
-        event_handler = ParamEventHandler() # start class that handles the event
+        event_handler = ParamEventHandler(self.pluginPath, self.corePath, self.PG_UI) # start class that handles the event
         observer = Observer()
         observer.schedule(event_handler, self.corePath, recursive=True)
         observer.start()
 
         while self.runstat:
             time.sleep(1)
+            try:
+                self.PG_UI.editbox[0]
+            except:
+                self.runstat = False
 
         observer.stop()
         observer.join()
--- RhinoInterface/ParameterGuard_UI/parameterGuard.py
+++ RhinoInterface/ParameterGuard_UI/parameterGuard.py
@@ -1,4 +1,5 @@
-import wx
+import wx, threading
+import guard
 import UI_settings as UI
 from Utilities import ini_worker
 
@@ -9,9 +10,7 @@ class parameterGuardUI(wx.Dialog):
         self.configFile = self.corePath + '\\' + configFile
         self.current_y_pxpos_elem = 0
 
-        parent = None
-
-        wx.Dialog.__init__(self, parent, title='MW Parameter guard', size=UI.WMAIN['size'],
+        wx.Dialog.__init__(self, None, title='MW Parameter guard', size=UI.WMAIN['size'],
                            style=wx.SYSTEM_MENU | wx.CAPTION | wx.CLOSE_BOX |
                                  wx.TAB_TRAVERSAL | wx.STAY_ON_TOP | wx.RESIZE_BORDER)
 
@@ -26,10 +25,13 @@ class parameterGuardUI(wx.Dialog):
         self.Centre()
         self.Show()
 
+
+
         section_list = ini_worker.get_sections_list_from_ini(self.configFile)
 
-
         param_indentation = 10
+
+        self.editbox = []
 
         for section in section_list:
 
@@ -48,6 +50,7 @@ class parameterGuardUI(wx.Dialog):
                 section_params = ini_worker.get_section_from_ini(self.configFile, section)
 
                 font = wx.Font(8, wx.DEFAULT, wx.NORMAL, wx.NORMAL)
+
                 for param, value in section_params.iteritems():
                     text = wx.StaticText(self,
                                   label=param,
@@ -56,24 +59,37 @@ class parameterGuardUI(wx.Dialog):
 
                     text.SetForegroundColour(UI.PARAMCOLOR['FG'])  # set text color
 
-
-                    editbox = wx.TextCtrl(self,
+                    self.editbox.append(wx.TextCtrl(self,
                                 value=str(value),
                                 pos=(UI.EBOX['pos'][0], UI.THEADERSTART['pos'][1] + self.current_y_pxpos_elem),
                                 size=UI.EBOX['size'],
-                                style=wx.TE_PROCESS_ENTER)
+                                style=wx.TE_PROCESS_ENTER))
 
-                    editbox.SetForegroundColour(UI.ECOLOR2['FG'])  # set color
-                    editbox.SetBackgroundColour(UI.ECOLOR2['BG'])  # set color
+                    self.editbox[-1].SetForegroundColour(UI.ECOLOR2['FG'])  # set color
+                    self.editbox[-1].SetBackgroundColour(UI.ECOLOR2['BG'])  # set color
 
                     self.current_y_pxpos_elem += 20
 
+        #self.scroll = wx.ScrolledWindow(self, 1)
+        #self.scroll.SetScrollbars(0, 1, 0, 1000)
 
     # ------------------------------------------------------------------------------------------------------------------
     def OnExit(self, event):
         self.Destroy()
 
 
-app = wx.App(False)
-PG = parameterGuardUI('D:\\MWAdditive', 'D:\\MWAdditive', 'Mesh.ini')
-app.MainLoop()
+def main():
+    app = wx.App(False)
+    pluginPath = 'C:\\MWAdditive'
+    corePath = 'C:\\MWAdditive'
+    configFile = 'Mesh.ini'
+    PG = parameterGuardUI('C:\\MWAdditive', 'C:\\MWAdditive', 'Mesh.ini')
+    GOC = guard.guard_of_changes(pluginPath, corePath, PG)
+    GOC.start()
+    app.MainLoop()
+
+
+
+if __name__== "__main__":
+    main()
+
